5f407dbba89e2926e60ce14f0c0a7d8839d4e0cc
aj <allyshub@gmail.com>
2015-03-04 13:14:29 -0800

    minor changes to fresh_tomatoes file -added main

--- moviesite/movie_details.py
+++ moviesite/movie_details.py
@@ -3,22 +3,28 @@ import fresh_tomatoes
 import csv
 
 
-movie_list=[]
+def show_mov_details():
 
-#open csv file with movie title and youtube url
-#build movie list to be passed to fresh tomatoes open_movie_pge func
+    movie_list=[]
 
-try:
-    with open('my_movie_list.csv', 'rb') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            movie = media.Movie(row[0], row[1])
-            movie.get_movie_details()
-            movie_list.append(movie)
+    #open csv file with movie title and youtube url
+    #build movie list to be passed to fresh tomatoes open_movie_page func
 
-except Exception, e:
-    print "Encountered error " + str(e) + " while attempting to process input csv file. check inputs, please. Exiting...."
+    try:
+        with open('my_movie_list.csv', 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                movie = media.Movie(row[0], row[1])
+                movie.get_movie_details()
+                movie_list.append(movie)
 
-else:
-    fresh_tomatoes.open_movies_page(movie_list)
+    except Exception, e:
+            print "Encountered error " + str(e) + " while attempting to process input csv file. check inputs, please. Exiting...."
+    else:
+        fresh_tomatoes.open_movies_page(movie_list)
+
+
+
+if __name__ == '__main__':
+    show_mov_details()
 
